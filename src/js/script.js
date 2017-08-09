a64a193c36905b7d049e7d6158bf05e02a91eaa2
Christopher Aaron S. De Guzman <christopher.deguzman@wideout.com>
2017-08-09 16:23:50 +0800

    update for mobile responsive

--- src/js/script.js
+++ src/js/script.js
@@ -6,18 +6,33 @@ var tagCanvasOptions = {
   wheelZoom: false,
   animTiming: "Linear",
   textColour: "#EFE5D2",
-  textHeight: 26,
-  textFont: "Quicksand, sans-serif",
-  weight: true
+  textHeight: tagCanvasFontSize(),
+  textFont: "Quicksand, sans-serif"
 };
 
 window.onload = function(){
   TagCanvas.Start("tagCanvas", "", tagCanvasOptions);
 }
 
+window.onresize = function(){
+  TagCanvas.Delete("tagCanvas");
+  tagCanvasOptions.textHeight = tagCanvasFontSize();
+  fitCanvasToContainer(document.getElementById("tagCanvas"));
+  TagCanvas.Start("tagCanvas", "", tagCanvasOptions);
+}
+
 function fitCanvasToContainer(canvas){
+  var ctx = canvas.getContext('2d');
   canvas.style.width = "100%";
   canvas.style.height = "100%";
-  canvas.width = canvas.offsetWidth;
-  canvas.height = canvas.offsetHeight;
+  ctx.canvas.width = document.getElementById("canvascontainer").offsetWidth;
+  ctx.canvas.height = document.getElementById("canvascontainer").offsetHeight;
+}
+
+function tagCanvasFontSize(){
+  if(window.innerWidth < 768){
+    return (window.innerWidth * window.innerHeight) / 14000;
+  }else{
+    return 30;
+  }
 }
