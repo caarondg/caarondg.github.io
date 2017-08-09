ae8c5a3445230d2b75f344b26257b009a5719252
Christopher Aaron S. De Guzman <christopher.deguzman@wideout.com>
2017-08-09 17:15:40 +0800

    optimize for github pages caching

--- src/js/scrollrevealoptions.js
+++
@@ -1,8 +0,0 @@
-window.sr = ScrollReveal();
-sr.reveal('.my-background .dis-tcell h1', { duration: 1500, origin: 'bottom', scale: 1, easing: 'ease'});
-sr.reveal('.my-testimonial .dis-tcell h1', { duration: 1500 });
-sr.reveal('.my-testimonial .dis-tcell .grid-item', { duration: 1500 }, 200);
-sr.reveal('.skillcopy', { scale: 1, duration: 1500, distance: '5em'});
-sr.reveal('.canvascontainer', { scale: 1, duration: 1500, distance: '5em'});
-sr.reveal('.my-works .dis-tcell h1', { duration: 1500 });
-sr.reveal('.my-works .dis-tcell .workitem', { duration: 1600, scale: 1 }, 200);
