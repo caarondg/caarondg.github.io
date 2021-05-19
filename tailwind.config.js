abd1f768803651c6ea5c3e68cfd3a11b8058f387
caarondg <deguzmanc.aaron@gmail.com>
2021-05-19 23:04:15 +0800

    add hero

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,20 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    container: {
+      center: true,
+      padding: '1.5rem',
+    },
+    fontFamily: {
+      'quicksand': ['Quicksand', 'sans-serif'],
+    },
+    colors: {
+      black: '#000',
+      white: '#fff',
+      red: {
+        100: '#BB4430',
+      },
+    },
     extend: {},
   },
   variants: {
