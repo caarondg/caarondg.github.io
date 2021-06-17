1e8acbf4b6e11f210fe30c240c33e0e513e869f5
caarondg <deguzmanc.aaron@gmail.com>
2021-06-17 21:26:24 +0800

    update styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,13 +7,24 @@ module.exports = {
       padding: '1.5rem',
     },
     fontFamily: {
-      'quicksand': ['Quicksand', 'sans-serif'],
+      'caveat': ['Caveat', 'cursive'],
+      'roboto': ['Roboto', 'sans-serif'],
     },
     colors: {
-      black: '#000',
+      black: {
+        100: '#000',
+        200: '#212545',
+      },
       white: '#fff',
-      red: {
-        100: '#BB4430',
+      gray: '#f5f5f5',
+      blue: {
+        100: '#242d73',
+        200: '#3b4594',
+        300: '#d2d5ee',
+      },
+      yellow: {
+        100: '#fde034',
+        200: '#fdf1cc',
       },
     },
     extend: {},
