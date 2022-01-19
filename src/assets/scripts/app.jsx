83d2802409d3d11aeec119774014c327fd20e9e8
caarondg <deguzmanc.aaron@gmail.com>
2022-01-19 20:52:20 +0800

    update theme colors and only show maintenance hero

--- src/assets/scripts/app.jsx
+++ src/assets/scripts/app.jsx
@@ -1,17 +1,19 @@
 import React from 'react';
 
+import Overlay from './components/overlay';
 import Hero from './components/hero';
-import Works from './components/works';
-import About from './components/about';
-import Footer from './components/footer';
+// import Works from './components/works';
+// import About from './components/about';
+// import Footer from './components/footer';
 
 const App = () => {
   return (
     <React.Fragment>
       <Hero />
-      <Works />
+      {/* <Works />
       <About />
-      <Footer />
+      <Footer /> */}
+      <Overlay />
     </React.Fragment>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -14,24 +14,28 @@ module.exports = {
       padding: '1.5rem',
     },
     fontFamily: {
-      'caveat': ['Caveat', 'cursive'],
+      'roboto-slab': ['Roboto Slab', 'serif'],
       'roboto': ['Roboto', 'sans-serif'],
     },
     colors: {
       black: {
         100: '#000',
-        200: '#212545',
+        200: '#0c0c0c',
+        300: '#1c1a1a',
       },
-      white: '#fff',
-      gray: '#f5f5f5',
+      white: {
+        100: '#fff',
+      },
+      gray: {
+        100: '#3f3d3c',
+      },
       blue: {
-        100: '#242d73',
-        200: '#3b4594',
-        300: '#d2d5ee',
+        100: '#173159',
+        200: '#264a6a',
+        300: '#697a91',
       },
-      yellow: {
-        100: '#fde034',
-        200: '#fdf1cc',
+      gold: {
+        100: '#b89a73',
       },
     },
     extend: {
@@ -41,6 +45,9 @@ module.exports = {
         '25': 25,
         '75': 75,
         '100': 100,
+      },
+      boxShadow: {
+        'gold-inset': 'inset 0 0 0 1px #b89a73',
       },
     },
   },
